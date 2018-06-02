4eac39dc619cefdba9a9174b4ad0ce950f8ec859
Anna <essekeranna1@gmail.com>
2018-06-02 22:08:27 +0300

    Выполнен третий урок

--- lesson-1/js/lesson.js
+++ lesson-1/js/lesson.js
@@ -1,28 +1,38 @@
-let money = prompt("Ваш бюджет на месяц?");
-let name = prompt("Название вашего магазина?");
-let time = 19;
+let money, name, time, price
+
+function start() {
+	money = prompt("Ваш бюджет на месяц?");
+	while (isNaN(money) || money == "" || money == null) {
+		money = prompt("Ваш бюджет на месяц?");
+	};
+	name = prompt("Название вашего магазина?").toUpperCase();
+}
+start();
 
 let mainList = {
 	budget: money,
 	shopName: name,
 	shopGoods: [],
 	employers: {},
-	open: false
+	open: false,
+	discount: true
 }
 
-for (let i = 0; i < 5; i++) {
+function chooseGoods() {
+	for (let i = 0; i < 5; i++) {
 
-	let a = prompt("Какой тип товаров будем продавать?");
+		let a = prompt("Какой тип товаров будем продавать?");
 
-	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
-		console.log('Всё верно!');
-		mainList.shopGoods[i] = a;
-	} else {
-		alert('Некорректно заполнена строка!');
-		i--;
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+			console.log('Всё верно!');
+			mainList.shopGoods[i] = a;
+		} else {
+			alert('Некорректно заполнена строка!');
+			i--;
+		}
 	}
 }
-
+chooseGoods();
 
 /*let i = 0;
 while (i < 4) {
@@ -51,17 +61,46 @@ do {
 }
 while (i < 4);*/
 
+function workTime(time) {
+	if (time < 0) {
+		console.log("Такого просто не может быть");
+	} else if (time > 8 && time < 20) {
+		console.log("Время работать!");
+	} else if (time < 24) {
+		console.log("Уже слишком поздно!");
+	} else {
+		console.log("В сутках только 24 часа!");
+	}
+}
+workTime(18);
 
-if (time < 0) {
-	console.log("Такого просто не может быть");
-} else if (time > 8 && time < 20) {
-	console.log("Время работать!");
-} else if (time < 24) {
-	console.log("Уже слишком поздно!");
-} else {
-	console.log("В сутках только 24 часа!");
-};
+function budgetDay() {
+	alert("Ваш бюджет на один день: " + (mainList.budget / 30));
+}
+budgetDay();
 
-alert("Ваш бюджет на один день: " + (mainList.budget / 30));
+function discountSystem(price) {
+	if (mainList.discount == true) {
+		alert("Цена со скидкой: " + (price / 100 * 80));
+	} else {
+		alert("Цена без скидки: " + price);
+	}
+}
+discountSystem(1000);
+
+function employers() {
+	for (let i = 0; i < 4; i++) {
+
+		let a = prompt("Введите имя сотрудника:");
+
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+		mainList.employers[i] = i + " - " + a;
+		} else {
+			alert('Некорректно заполнена строка!');
+			i--;
+		}
+	}
+}
+employers();
 
 console.log(mainList);
---
+++ lesson-3dop/js/dop.js
@@ -0,0 +1,51 @@
+let str = "урок-3-был слишком легким";
+
+str = str.charAt(0).toUpperCase() + str.substr(1);
+console.log(str);
+
+str = str.replace(/\-/g, " ");
+console.log(str);
+
+str = str.slice(19,26);
+console.log(str);
+
+str = str.slice(0,4) + "о";
+console.log(str);
+alert(str);
+
+let arr = [20,33,1,"Человек",2,3];
+let newArr = [];
+let sum = 0;
+
+for (i = 0; i < arr.length; i++) {
+	if (typeof(arr[i]) === "number") {
+			let a = Math.pow(arr[i],3);
+			newArr.push(a);
+	} else {
+		delete arr[i];
+	}
+}
+
+for (let j = 0; j < newArr.length; j++) {
+	sum += newArr[j];
+}
+
+sum = Math.sqrt(sum);
+console.log(sum);
+
+let a;
+function newFunction(a) {
+	for (let i = 0; i < 1; i++) {
+
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+		console.log(a.trim());
+		} else if (a.length > 50) {
+			a = a.trim();
+			a = a.substr(0,50) + "...";
+			console.log(a);
+		} else {
+			alert('Некорректно заполнена строка!');
+		}
+	}
+}
+newFunction("    СтрокаСтрокаСтрокаСтрокаСтрокаСтрокаСтрокаСтрокаСтрокаСтрокаСтрокаСтрока     ");
