3f7e14a968a6b9bdd2f67e7895502a25f90a0561
Anna <essekeranna1@gmail.com>
2018-06-14 14:55:18 +0300

    Выполнен шестой урок

--- lesson-5/second/js/script.js
+++ lesson-5/second/js/script.js
@@ -1,41 +1,208 @@
-let btnOpen = document.getElementById('open-btn'),
-				name = document.getElementsByClassName('name-value'),
-				budget = document.getElementsByClassName('budget-value'),
-				goods = document.getElementsByClassName('goods-value'),
-				items = document.getElementsByClassName('items-value'),
-				employers = document.getElementsByClassName('employers-value'),
-				discount = document.getElementsByClassName('discount-value'),
-				isopen = document.getElementsByClassName('isopen-value'),
-				goodsItem = document.getElementsByClassName('goods-item'),
-				button = document.getElementsByTagName('button'),
-				chooseItems = document.querySelector('.choose-item'),
-				time = document.querySelector('.time-value'),
-				countBudget = document.querySelector('.count-budget-value'),
-				hireEmployersItem = document.querySelectorAll('.hire-employers-item');
+let open = document.getElementById('open-btn'),
+	name_value = document.getElementsByClassName('name-value')[0],
+	budget_value = document.getElementsByClassName('budget-value')[0],
+	goods_value = document.getElementsByClassName('goods-value')[0],
+	items_value = document.getElementsByClassName('items-value')[0],
+	employers_value = document.getElementsByClassName('employers-value')[0],
+	discount_value = document.getElementsByClassName('discount-value')[0],
+	isopen_value = document.getElementsByClassName('isopen-value')[0],
 
-console.log(btnOpen);
+	goods_item = document.getElementsByClassName('goods-item'),
+	goods_btn = document.getElementsByTagName('button')[1],
+	budget_btn = document.getElementsByTagName('button')[2],
+	employers_btn = document.getElementsByTagName('button')[3],
+	choose_item = document.querySelector('.choose-item'),
+	time_value = document.querySelector('.time-value'),
+	count_budget_value = document.querySelector('.count-budget-value'),
+	hire_employers_item = document.querySelectorAll('.hire-employers-item');
 
-console.log(name[0]);
-console.log(budget[0]);
-console.log(goods[0]);
-console.log(items[0]);
-console.log(employers[0]);
-console.log(discount[0]);
-console.log(isopen[0]);
 
-console.log(goodsItem[0]);
-console.log(goodsItem[1]);
-console.log(goodsItem[2]);
-console.log(goodsItem[3]);
+let money, price;
 
-console.log(button[1]);
-console.log(button[2]);
-console.log(button[3]);
+open.addEventListener('click', () => {
+	do {
+		money = prompt("Ваш бюджет на месяц?", "");
+	}
+	while (isNaN(money) || money == "" || money == null);
+	budget_value.textContent = money;
+	name_value.textContent = prompt("Название вашего магазина?", "").toUpperCase();
 
-console.log(chooseItems);
-console.log(time);
-console.log(countBudget);
+	price = confirm("Будем активировать систему скидок?");
 
-console.log(hireEmployersItem[0]);
-console.log(hireEmployersItem[1]);
-console.log(hireEmployersItem[2]);
+	if (price === true) {
+		mainList.discount = true;
+		discount_value.style.backgroundColor = 'green';
+	} else {
+		mainList.discount = false;
+		discount_value.style.backgroundColor = 'red';
+	}
+
+});
+
+
+choose_item.addEventListener('change', () => {
+	let items = choose_item.value;
+	if (isNaN(items) && items != '') {
+		mainList.shopItems = items.split(', ');
+		mainList.shopItems.sort();
+		items_value.textContent = mainList.shopItems;
+	}
+});
+
+// блокируем кнопки
+goods_btn.disabled = true;
+budget_btn.disabled = true;
+employers_btn.disabled = true;
+
+// пишем функцию блокировки инпутов
+function disabledInp (bool){
+	
+	choose_item.disabled = bool;
+	count_budget_value.disabled = bool;
+	for(let i = 0; i < goods_item.length; i++){
+		goods_item[i].disabled = bool;
+	}
+
+	for(let i = 0; i < hire_employers_item.length; i++){
+		hire_employers_item[i].disabled = bool;
+	}
+}
+disabledInp (true);
+// пишем функцию блокировки инпутов
+
+time_value.addEventListener('change', () => {
+	let time = time_value.value;
+
+	if (time < 0 || time == '') {
+		console.log("Такого просто не может быть");
+		mainList.open = false;
+	} else if (time > 8 && time < 20) {
+		console.log("Время работать!");
+		mainList.open = true;
+	} else if (time < 24) {
+		console.log("Уже слишком поздно!");
+		mainList.open = false;
+	} else {
+		console.log("В сутках только 24 часа!");
+		mainList.open = false;
+	}
+
+	if (mainList.open === true) {
+		isopen_value.style.backgroundColor = 'green';
+	} else {
+		isopen_value.style.backgroundColor = 'red';
+	}
+		
+// по инпутам
+	if (mainList.open === true) {
+		// вызываем функцию с значением ложь
+		disabledInp (false);
+		budget_btn.disabled = false;
+		count_budget_value.disabled = true;
+	} 
+});
+
+// по кнопкам
+for (let i = 0; i < goods_item.length; i++) {
+	 goods_item[i].addEventListener('change', ()=>{
+		
+			if (goods_item[0].value === "" || goods_item[1].value === "" || goods_item[2].value === "" ||goods_item[3].value === "") {
+				goods_btn.disabled = true;
+			} else {
+				goods_btn.disabled = false;
+			}
+		
+	});
+}
+
+for (let i = 0; i < hire_employers_item.length; i++) {
+	 hire_employers_item[i].addEventListener('change', ()=>{
+		
+			if (hire_employers_item[0].value === "" || hire_employers_item[1].value === "" || hire_employers_item[2].value === "") {
+				employers_btn.disabled = true;
+			} else {
+				employers_btn.disabled = false;
+			}
+		
+	});
+}
+
+let mainList = {
+	budget: money,
+	shopName: name,
+	shopGoods: [],
+	employers: [],
+	open: false,
+	discount: true,
+	shopItems: []
+};
+
+budget_btn.addEventListener('click', () => {
+	count_budget_value.value = money / 30;
+});
+
+
+goods_btn.addEventListener('click', () => {
+	for (let i = 0; i < goods_item.length; i++) {
+		let a = goods_item[i].value;
+		if ((typeof(a)) === 'string' && (typeof(a)) != null &&  a.length < 50) {
+			console.log('Всё верно!');
+			mainList.shopGoods[i] = a;
+			goods_value.textContent = mainList.shopGoods;
+		} else {
+			alert('Некорректно заполнена строка!');
+			i--;
+		}
+	}
+});
+
+employers_btn.addEventListener ('click', () => {
+	for (let i = 0; i < hire_employers_item.length; i++) {
+		let name = hire_employers_item[i].value;
+		mainList.employers[i] = name;
+		employers_value.textContent = mainList.employers;
+	}
+});
+
+for (let i = 0; i < 3; i++) {
+	hire_employers_item[i].addEventListener("change", () => {
+		let pattern = /[^а-яА-ЯёЁ]/;
+
+		if (pattern.test(hire_employers_item[i].value)) hire_employers_item[i].value = "";
+	});
+}
+
+
+
+/*let i = 0;
+while (i < 4) {
+	let a = prompt("Какой тип товаров будем продавать?");
+	i++;
+	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+		console.log('Всё верно!');
+		mainList.shopGoods[i] = a;
+	} else {
+		alert('Некорректно заполнена строка!');
+		i--;
+	}
+}*/
+
+/*let i = 0;
+do {
+	let a = prompt("Какой тип товаров будем продавать?");
+	i++;
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+		console.log('Всё верно!');
+		mainList.shopGoods[i] = a;
+	} else {
+		alert('Некорректно заполнена строка!');
+		i--;
+	}
+}
+while (i < 4);*/
+
+/*for (let key in mainList) {
+	alert("Наш магазин включает в себя: " + key);
+}*/
+
+console.log(mainList);
