65d42f92279b89851f8dbdd90b9c4c56f81098bc
Anna <essekeranna1@gmail.com>
2018-06-18 23:03:40 +0300

    Выполнен восьмой урок

--- lesson-7/first/js/script.js
+++ lesson-7/first/js/script.js
@@ -33,4 +33,54 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	});
 
+//Timer
+let deadline = '2018-06-19';
+function getTimeRemaining(endtime) {
+	let t = Date.parse(endtime) - Date.parse(new Date()),
+					seconds = Math.floor((t / 1000) % 60),
+					minutes = Math.floor ((t / 1000 / 60) % 60),
+					hours = Math.floor((t / (1000 * 60 * 60)));
+
+					return {
+						'total': t,
+						'hours': hours,
+						'minutes': minutes,
+						'seconds': seconds
+					};
+};
+
+function setClock(id, endtime) {
+	let timer = document.getElementById(id),
+					hours = timer.querySelector('.hours'),
+					minutes = timer.querySelector('.minutes'),
+					seconds = timer.querySelector('.seconds');
+
+	function updateClock() {
+		let t = getTimeRemaining(endtime);
+		hours.innerHTML = addZero(t.hours);
+		minutes.innerHTML = addZero(t.minutes);
+		seconds.innerHTML = addZero(t.seconds);
+
+		function addZero(i) {
+			return (i < 10) ? '0' + i : i;
+		}
+
+
+		if (t.total <= 0) {
+			clearInterval(timeInterval);
+			hours.innerHTML = '00';
+			minutes.innerHTML = '00';
+			seconds.innerHTML = '00';
+		}
+
+
+	};
+
+	updateClock();
+	var timeInterval = setInterval(updateClock, 1000);
+
+};
+
+setClock('timer', deadline);
+
 });
