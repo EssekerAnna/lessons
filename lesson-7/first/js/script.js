e5413bdccf348b4595835f328dd4fe853fcfe4f1
Anna <essekeranna1@gmail.com>
2018-07-08 00:09:43 +0300

    12-13 урок

--- lesson-7/first/js/script.js
+++ lesson-7/first/js/script.js
@@ -35,7 +35,7 @@ let showTabContent = (b) => {
 	});
 
 //Timer
-let deadline = '2018-06-23';
+let deadline = '2018-06-22';
 let getTimeRemaining = (endtime) => {
 	let t = Date.parse(endtime) - Date.parse(new Date()),
 					seconds = Math.floor((t / 1000) % 60),
@@ -195,6 +195,123 @@ formCont.addEventListener('submit', function(event) {
 		// Очищаем поля ввода
 	}
 });
+
+// Slider
+
+let slideIndex = 1,
+				slides = document.getElementsByClassName('slider-item'),
+				prev = document.querySelector('.prev'),
+				next = document.querySelector('.next'),
+				dotsWrap = document.querySelector('.slider-dots'),
+				dots = document.getElementsByClassName('dot');
+
+				showSlides(slideIndex);
+
+				function showSlides(n) {
+					if (n > slides.length) {
+						slideIndex = 1;
+					};
+					if (n < 1) {
+						slideIndex = slides.length;
+					};
+
+					for (let i = 0; i < slides.length; i++) {
+						slides[i].style.display = 'none';
+					}
+
+					for (let i = 0; i < dots.length; i++) {
+						dots[i].classList.remove('dot-active');
+					}
+
+					slides[slideIndex - 1].style.display = 'block';
+					dots[slideIndex - 1].classList.add('dot-active');
+				}
+
+				function plusSlides(n) {
+					showSlides(slideIndex += n);
+				}
+
+				function currentSlide(n) {
+					showSlides(slideIndex = n);
+				}
+
+				prev.addEventListener('click', function() {
+					plusSlides(-1);
+				});
+
+				next.addEventListener('click', function() {
+					plusSlides(1);
+				});
+
+				dotsWrap.addEventListener('click', function(event) {
+					for (let i = 0; i < dots.length + 1; i++) {
+						if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+							currentSlide(i);
+						}
+					}
+				});
+
+// Calc
+
+let persons = document.getElementsByClassName('counter-block-input')[0],
+				restDays = document.getElementsByClassName('counter-block-input')[1],
+				place = document.getElementById('select'),
+				totalValue = document.getElementById('total'),
+				personsSum = 0,
+				daysSum = 0,
+				total = 0;
+
+				totalValue.innerHTML = 0;
+
+
+				persons.addEventListener('change', function () {
+
+					personsSum = +this.value;
+
+					if (restDays.value == '') {
+						totalValue.innerHTML = 0;
+					} else {
+						totalValue.innerHTML = total;
+					}
+				});
+
+				if ((personsSum ^ 0) === personsSum) {
+						total = (daysSum + personsSum) * 4000;
+					} else {
+						alert('Нельзя вводить дроби!');
+						persons.value = '';
+					}
+
+				restDays.addEventListener('change', function () {
+					
+					daysSum = +this.value;
+
+					if ((daysSum ^ 0) === daysSum) {
+						total = (daysSum + personsSum) * 4000;
+					} else {
+						alert('Нельзя вводить дроби!');
+						restDays.value = '';
+					}
+
+					if (persons.value == '') {
+						totalValue.innerHTML = 0;
+					} else {
+						totalValue.innerHTML = total;
+					}
+				});
+
+				place.addEventListener('change', function() {
+					if (restDays.value == '' || persons.value == '') {
+						totalValue.innerHTML = 0;
+					} else {
+						let a = total;
+						totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+					}
+				});
+
+				function isInteger(num) {
+				  return (num ^ 0) === num;
+				}
 
 
 });
